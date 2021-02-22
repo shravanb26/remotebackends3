6333e4f7446afae61d95bfc5f01d2aac5060c277
shravanb26 <63107861+shravanb26@users.noreply.github.com>
2021-02-22 09:45:30 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-# output "vpc-id" {
-#   value = "${aws_vpc.default.id}"
-# }
+ output "vpc-id" {
+   value = "${aws_vpc.default.id}"
+ }
 
 # output "ec2-id" {
 #   value = "${aws_instance.web-1.id}"
